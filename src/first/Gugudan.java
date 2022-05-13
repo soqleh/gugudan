77a4af21e20b98a35bb510f5e8f5047235475427
soqleh <soqleh@naver.com>
2022-05-13 14:30:39 +0900

    feat: add my gugu81 program

--- src/first/Gugudan.java
+++ src/first/Gugudan.java
@@ -2,10 +2,9 @@ package first;
 
 import java.util.Scanner;
 
-public class Gugu81 {
+public class Gugudan {
     public static void main(String[] args){
 
-        final int multi_value = 9;
         final int min_gugudan = 2;
         final int max_gugudan = 9;
 
@@ -14,17 +13,15 @@ public class Gugu81 {
         System.out.print("[안내]희망하는 구구단을 숫자로 입력해 주세요 (2 ~ 9) : ");
 
         int dan = s.nextInt();
+        s.close(); //scanner종료
         System.out.printf("\n%d 단 이 입력되었습니다.\n",dan);
-
         if(min_gugudan > dan || dan > max_gugudan){
             System.out.println("[경고]구구단은 2단 ~ 9단 까지만 선택할 수 있습니다.\n프로그램을 종료합니다.");
-            s.close();
             return;
         }
-
-        for (int i = 1; i <= multi_value; ++i){
+        //어차피 구구단이라 오히려 변수보다 1부터 9까지 곱하는게 더 가독성이 좋은 것 같다.
+        for (int i = 1; i <= 9; ++i){
             System.out.println(dan + " * " + i + " = " + (dan * i));
         }
-        s.close();
     }
 }
---
+++ src/my/Gugu81.java
@@ -0,0 +1,34 @@
+package my;
+
+import java.util.Scanner;
+
+public class Gugu81 {
+    public static void main(String[] args) {
+
+        final int min_gugudan = 2;
+        final int max_gugudan = 9;
+
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            System.out.print("[안내]희망하는 구구단을 숫자로 입력해 주세요 (2 ~ 9) : ");
+
+            int dan = sc.nextInt();
+
+            System.out.printf("\n%d 단 이 입력되었습니다.\n", dan);
+            if (min_gugudan > dan || dan > max_gugudan) {
+                System.out.println("[경고]구구단은 2단 ~ 9단 까지만 선택할 수 있습니다.\n다시 입력해주세요..\n\n");
+                continue;
+            }
+            //어차피 구구단이라 오히려 변수보다 1부터 9까지 곱하는게 더 가독성이 좋은 것 같다.
+            for (int i = 1; i <= 9; ++i) {
+                System.out.println(dan + " * " + i + " = " + (dan * i));
+            }
+            System.out.println("계속 진행할까요? y/n");
+            String check = sc.next();
+            if(check.equals("n")) running = false;
+        }
+        System.out.println("[안내] 프로그램을 종료합니다.");
+        sc.close(); //scanner종료
+    }
+}
