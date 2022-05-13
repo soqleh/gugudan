250a5a2966406d06d86169b7e190e23642597cd4
soqleh <soqleh@naver.com>
2022-05-13 14:32:53 +0900

    style: 주석 추가

--- src/my/Gugu81.java
+++ src/my/Gugu81.java
@@ -3,6 +3,8 @@ package my;
 import java.util.Scanner;
 
 public class Gugu81 {
+    //사용자가 프로그램을 종료할 때 까지 계속해서 구동되고
+    //사용자가 입력 범위를 잘못 입력하면 다시 재 입력을 받는 프로그램
     public static void main(String[] args) {
 
         final int min_gugudan = 2;
@@ -18,7 +20,7 @@ public class Gugu81 {
             System.out.printf("\n%d 단 이 입력되었습니다.\n", dan);
             if (min_gugudan > dan || dan > max_gugudan) {
                 System.out.println("[경고]구구단은 2단 ~ 9단 까지만 선택할 수 있습니다.\n다시 입력해주세요..\n\n");
-                continue;
+                continue;//다시 재 입력 받기
             }
             //어차피 구구단이라 오히려 변수보다 1부터 9까지 곱하는게 더 가독성이 좋은 것 같다.
             for (int i = 1; i <= 9; ++i) {
@@ -26,7 +28,7 @@ public class Gugu81 {
             }
             System.out.println("계속 진행할까요? y/n");
             String check = sc.next();
-            if(check.equals("n")) running = false;
+            if(check.equals("n")) running = false;//종료를 위해 while 조건을 빠져나온다.
         }
         System.out.println("[안내] 프로그램을 종료합니다.");
         sc.close(); //scanner종료
